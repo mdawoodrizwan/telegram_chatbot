ef305878787142e8f8257a11f55d94be30783856
mdawoodrizwan <dawood.rizwan@kryptomind.com>
2024-10-17 12:04:45 +0500

    fix:

--- bot.py
+++ bot.py
@@ -45,8 +45,6 @@ async def sql(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def java(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Tutorial link: https://youtu.be/i6AZdFxTK9I")
 
-async def skillup(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Tutorial link: https://www.simplilearn.com/?&utm_source=google&utm_medium=cpc&utm_term=%2Bwww%20%2Bsimplilearn%20%2Bcom&utm_content=803350713-40537012023-467574577661&utm_device=c&utm_campaign=Search-Brand-Broad-IN-AllDevice-adgroup-brand-navigation&gclid=Cj0KCQjw0oyYBhDGARIsAMZEuMv5mA9EysZZ5NfhK65Cb5OU0Q0TVC4con6DQzHF502-dfgA7QQFCgQaAu5sEALw_wcB")
 
 async def contact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("You can contact us at the official email id.")
@@ -61,7 +59,6 @@ application.add_handler(CommandHandler('content', content))
 application.add_handler(CommandHandler('Python', python))
 application.add_handler(CommandHandler('SQL', sql))
 application.add_handler(CommandHandler('Java', java))
-application.add_handler(CommandHandler('Skillup', skillup))
 application.add_handler(CommandHandler('contact', contact))
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
